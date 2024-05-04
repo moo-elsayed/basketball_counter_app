79032620aa6abe23e15b63e60799ac13c83cc35f
kk28xd <159706787+kk28xd@users.noreply.github.com>
2024-05-04 10:09:32 +0300

    first commit

---
+++ lib/constants.dart
@@ -0,0 +1,4 @@
+import 'package:flutter/material.dart';
+
+const KPrimaryColor = Color(0xffFF9900);
+const KSecondaryColor = Color(0xff023047);
---
+++ lib/cubit/counter_states.dart
@@ -0,0 +1,9 @@
+abstract class CounterStates {}
+
+class CounterInitialState extends CounterStates {}
+
+class CounterAIncrementState extends CounterStates {}
+
+class CounterBIncrementState extends CounterStates {}
+
+class PointsReset extends CounterStates {}
